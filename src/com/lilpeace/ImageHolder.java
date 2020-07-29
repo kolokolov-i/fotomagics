25abbf3e658dccf16bf1d74ee614811665931943
superBro <delocksuperbro@gmail.com>
2020-07-30 00:54:18 +0700

    picture fit

--- src/com/lilpeace/ImageHolder.java
+++ src/com/lilpeace/ImageHolder.java
@@ -2,20 +2,52 @@ package com.lilpeace;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class ImageHolder extends JPanel {
 
-    Image image;
+    BufferedImage image;
 
-    public void setImage(Image image) {
+    public ImageHolder() {
+        setBackground(Color.WHITE);
+    }
+
+    public void setImage(BufferedImage image) {
         this.image = image;
     }
 
     @Override
     public void paint(Graphics g) {
-//        super.paint(g);
-//        Toolkit tk = Toolkit.getDefaultToolkit();
-//        Image img = tk.getImage("pic1.jpg");
-        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+        super.paint(g);
+        if(image == null) return;
+        int
+                panelW = getWidth(),
+                panelH = getHeight(),
+                picW = image.getWidth(),
+                picH = image.getHeight(),
+                resX, resY, resW, resH;
+        if(picW < panelW && picH < panelH){
+            resW = picW;
+            resH = picH;
+            resX = panelW / 2 - resW / 2;
+            resY = panelH / 2 - resH / 2;
+        }
+        else {
+            float mW, mH, mR;
+            mW = ((float) panelW) / picW;
+            mH = ((float) panelH) / picH;
+            mR = Math.min(mW, mH);
+            resW = (int) (picW * mR);
+            resH = (int) (picH * mR);
+            if(mW < mH){
+                resX = 0;
+                resY = (int) (panelH / 2 - (picH * mR / 2));
+            }
+            else{
+                resX = (int) (panelW / 2 - (picW * mR / 2));
+                resY = 0;
+            }
+        }
+        g.drawImage(image, resX, resY, resW, resH, this);
     }
 }
